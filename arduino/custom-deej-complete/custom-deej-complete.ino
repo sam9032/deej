5fa841c9b941a15c2fdd24d8b8e53e26795c5a8d
sam9032 <sam.peters@live.de>
2021-04-17 00:52:36 +0200

    work in progress. Trying to merge with https://github.com/SNR-Tech-Bytes/deej_encoder.

--- arduino/custom-deej-complete/custom-deej-complete.ino
+++ arduino/custom-deej-complete/custom-deej-complete.ino
@@ -1,28 +1,34 @@
 #include <Arduino.h>
-#include <ESP32Encoder.h>
-#include <FastLED.h>
+#include <RotaryEncoder.h>
+//#include <FastLED.h>
 
 // LED setup:
 // Number of LEDs (generally same as NUM_SLIDERS):
-#define NUM_LEDS 5
+//#define NUM_LEDS 5
 // LED control pin:
-#define DATA_PIN 2
-#define ledBrightness 50
-#define minHue 14
-#define maxHue 102
-#define mutecolor Red
-#define colorCorrectionMode Typical8mmPixel
-CRGB leds[NUM_LEDS];
+//#define DATA_PIN 2
+//#define ledBrightness 50
+//#define minHue 14
+//#define maxHue 102
+//#define mutecolor Red
+//#define colorCorrectionMode Typical8mmPixel
+//CRGB leds[NUM_LEDS];
 
-const int NUM_SLIDERS = 5;
+const int NUM_SLIDERS = 4;
 int analogSliderValues[NUM_SLIDERS];
 
 // avoid using pins with LEDs attached
 // buttons pins:
-const int button_pin[NUM_SLIDERS] = {27, 33, 35, 32, 34};
-int encodertoLedHue[NUM_SLIDERS];
+const int button_pin[NUM_SLIDERS] = {4,7,10,13};
+//int encodertoLedHue[NUM_SLIDERS];
 
-ESP32Encoder encoder[NUM_SLIDERS];
+//ESP32Encoder encoder[NUM_SLIDERS];
+RotaryEncoder encoder1(2,3);
+RotaryEncoder encoder2(5,6);
+RotaryEncoder encoder3(8,9);
+RotaryEncoder encoder4(11,12);
+
+
 
 int knobState[NUM_SLIDERS];
 int Mute[NUM_SLIDERS];
@@ -34,19 +40,18 @@ void checkButtons();
 void checkEncoders();
 
 void setup() {
-    LEDS.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS)
-        .setCorrection(colorCorrectionMode);
-    LEDS.setBrightness(ledBrightness);
+//    LEDS.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS)
+//        .setCorrection(colorCorrectionMode);
+//    LEDS.setBrightness(ledBrightness);
 
     Serial.begin(9600);
 
-    ESP32Encoder::useInternalWeakPullResistors = UP;
+//    ESP32Encoder::useInternalWeakPullResistors = UP;
     // set encoder pins A & B (CLK & DT) here:
-    encoder[0].attachHalfQuad(15, 16);
-    encoder[1].attachHalfQuad(17, 18);
-    encoder[2].attachHalfQuad(25, 26);
-    encoder[3].attachHalfQuad(19, 21);
-    encoder[4].attachHalfQuad(22, 23);
+    encoder[0].attachHalfQuad(2, 3);
+    encoder[1].attachHalfQuad(5, 6);
+    encoder[2].attachHalfQuad(8, 9);
+    encoder[3].attachHalfQuad(11, 12);
 
     for (int i = 0; i < NUM_SLIDERS; i++) {
         analogSliderValues[i] = 512;
@@ -71,7 +76,7 @@ void loop() {
     checkEncoders();
     sendSliderValues();
     // printSliderValues();//debug
-    FastLED.show();
+    //FastLED.show();
 }
 
 void sendSliderValues() {
@@ -121,22 +126,22 @@ void checkEncoders() {
         if (knobVal[i] > 0 && knobVal[i] < 102 && Mute[i] == 0) {
             // Normal volume levels
             analogSliderValues[i] = map(knobVal[i], 0, 102, 0, 1023);
-            encodertoLedHue[i] = map(knobVal[i], 0, 102, minHue, maxHue);
-            leds[i].setHue(encodertoLedHue[i]);
+//            encodertoLedHue[i] = map(knobVal[i], 0, 102, minHue, maxHue);
+//            leds[i].setHue(encodertoLedHue[i]);
         } else if (Mute[i] == 0 && (knobVal[i] >= 102)) {
             // constrain to max volume
             analogSliderValues[i] = 1023;
             encoder[i].setCount(102);
-            leds[i].setHue(maxHue);
+//            leds[i].setHue(maxHue);
         } else if (Mute[i] == 1) {
             // is muted, output volume of 0
             analogSliderValues[i] = 0;
-            leds[i] = CRGB::mutecolor;
+ //           leds[i] = CRGB::mutecolor;
         } else {
             // not muted, but volume = 0
             analogSliderValues[i] = 0;
             encoder[i].setCount(0);
-            leds[i].setHue(minHue);
+//            leds[i].setHue(minHue);
             // minHue is probably better since
             // we aren't actually muted
         }
